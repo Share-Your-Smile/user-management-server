0f2fc27489c773214e5c621d657fb17521886c77
yason0319 <yason0319@gmail.com>
2020-11-28 19:59:27 +0900

    add qrcode(base64)

--- api/qrcode/generate-qrcode.js
+++ api/qrcode/generate-qrcode.js
@@ -1,21 +1,51 @@
 const QRcode = require('qrcode');
 
+const getBySvg = (req, res) => {
+  const options = {
+    type: 'svg'
+  }
+  console.log(req.body.url);
+  QRcode.toString(req.body.url, options, function (err, str) {
+    console.log('callback result');
+    console.log(err);
+    res.status(200);
+    res.type('json');
+    res.send({
+      result: str
+    });
+  });
+}
+
+const getByBase64 = (req, res) => {
+  QRcode.toDataURL(req.body.url, function (err, str) {
+    if (err !== null) {
+      throw new Error('GET_ERROR');
+    }
+    res.status(200);
+    res.type('json');
+    res.send({
+      result: str
+    });
+  });
+}
+
 const generateQrcode = (req, res) => {
   try {
-    const options = {
-      type: 'svg'
+    switch (req.body.type) {
+      case 'svg':
+        getBySvg(req, res);
+        break;
+      case 'base64':
+        getByBase64(req, res);
+        break;
+      default:
+        throw new Error('TYPE_NOT_SET');
     }
-    console.log(req.body.url);
-    QRcode.toString(req.body.url, options, function (err, str) {
-      console.log('callback result');
-      console.log(err);
-      res.status(200);
-      res.type('json');
-      res.send({
-        result: str
-      });
-    })
+    
   } catch (err) {
+    if (err !== null) {
+      throw new Error('GET_ERROR');
+    }
     res.status(400);
     res.type('json');
     res.send({
